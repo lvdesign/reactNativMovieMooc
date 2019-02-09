743872fe025508fa6d74509fa9e3e35050eb08b7
lvdesign <mailbox@lvdesign.com.fr>
2019-02-09 18:35:18 +0100

    Movie React Native One Nav

---
+++ API/TMDBApi.js
@@ -0,0 +1,23 @@
+// API/TMDBApi.js
+
+const API_TOKEN = "579e65bf91757e3ac866d2f15390ef4d";
+
+export function getFilmsFromApiWithSearchedText (text, page) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
+
+
+
+export function getImageFromApi(name){
+  return 'https://image.tmdb.org/t/p/w300' + name ;
+}
+
+// Récupération du détail d'un film
+export function getFilmDetailFromApi (id) {
+  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
